70e69d3fba267e7a040336f50584195fa2a696ca
jornDS <117446313+Rackillu@users.noreply.github.com>
2024-05-25 22:39:45 +0200

    ability to clear local storage
    je kan nu de local storage clearen door een knop met daarna een popup.

--- js/favorieten.js
+++ js/favorieten.js
@@ -14,8 +14,36 @@ document.addEventListener('DOMContentLoaded', () => {
         `;
         tabelBody.appendChild(rij);
     });
+    if (tabelBody.children.length > 0) {
+        document.getElementById('clear').style.display = 'block';
+    }else{
+        document.getElementById('clear').style.display = 'none';
+    }
 });
 
+const clearButton = document.getElementById('clear');
+const popup = document.getElementById('popup');
+const cancelButton = document.getElementById('cancel');
+const deleteButton = document.getElementById('delete');
+
+clearButton.addEventListener('click', () => {
+    popup.classList.add('active');
+});
+
+cancelButton.addEventListener('click', () => {
+    popup.classList.remove('active');
+});
+
+deleteButton.addEventListener('click', () => {
+    clearLocalStorage();
+    popup.classList.remove('active');
+    document.getElementById('clear').style.display = 'none';
+});
+
+function clearLocalStorage() {
+    localStorage.clear();
+    location.reload();
+}
 
 
 
