c22ec8f2cfc50d5e2040957db70ca611a2b57b33
jornDS <117446313+Rackillu@users.noreply.github.com>
2024-05-25 18:45:27 +0200

    Country api added
    nu kan je landen opzoeken en data zoals  populatie hoofdstad, currency, afkortingen terug krijgen.

---
+++ js/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+let form = document.getElementById('form');
+let landInput = document.getElementById('land');
+let populatieCheckbox = document.getElementById('populatie');
+let afkortingCheckbox = document.getElementById('afkorting');
+let hoofdstadCheckbox = document.getElementById('hoofdstad');
+let currencyCheckbox = document.getElementById('currency');
+let resultaat = document.getElementById('resultaat');
+
+
+form.addEventListener('submit', submitForm);
+
+async function submitForm(e) {
+    e.preventDefault();
+    
+    const ingevoerdeLand = landInput.value;
+
+    const { name, population, abbreviation, capital, currency, media } = await getData(ingevoerdeLand);
+
+    if (name) {
+        let resultaatTekst = '';
+        resultaatTekst += `<h2>${name} <img src="${media.flag}" alt="Vlag van ${name}"></h2>`;
+
+        if (populatieCheckbox.checked) {
+            const geformatteerdePopulatie = formatteerPopulatie(population);
+            resultaatTekst += `<p><strong>Populatie:</strong> ${geformatteerdePopulatie}</p>`;
+        }
+
+        if (afkortingCheckbox.checked) {
+            resultaatTekst += `<p><strong>Afkorting:</strong> ${abbreviation}</p>`;
+        }
+
+        if (hoofdstadCheckbox.checked) {
+            resultaatTekst += `<p><strong>Hoofdstad:</strong> ${capital}</p>`;
+        }
+
+        if (currencyCheckbox.checked) {
+            resultaatTekst += `<p><strong>Valuta:</strong> ${currency}</p>`;
+        }
+        
+
+        resultaat.innerHTML = resultaatTekst;
+        document.getElementById('knop').style.display = 'block';
+
+    } else {
+        resultaat.textContent = 'Gegevens niet beschikbaar voor dit land.';
+        document.getElementById('knop').style.display = 'none';
+    }
+}
+
+
+async function getData(landNaam) {
+    return new Promise((resolve, reject) => {
+        fetch('https://api.sampleapis.com/countries/countries')
+            .then(resp => resp.json())
+            .then(landen => {
+                const gevondenLand = landen.find(land => land.name.toLowerCase() === landNaam.toLowerCase());
+                if (gevondenLand) {
+                    resolve(gevondenLand);
+                } else {
+                    resolve(null);
+                }
+            })
+            .catch(error => {
+                console.error('Fout bij het ophalen van data:', error);
+                reject(error);
+            });
+    });
+}
+
+function formatteerPopulatie(populatie) {
+    return populatie.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
